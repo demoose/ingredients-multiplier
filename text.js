7d664fa8a617ae4254359cb9d114ca1e24e2964a
demoose <mryuim@gmail.com>
2020-08-09 13:17:25 +0100

    Upload initial files

---
+++ text.js
@@ -0,0 +1,36 @@
+const result = document.querySelector('.result');
+const textArea = document.querySelector('[name="text"]');
+const calculateBtn = document.querySelector('.submit');
+const quantitySelector = document.querySelector('[name="quantity"]');
+
+function transformText(text, quantity) {
+  if (!quantity || !text.length) {
+    return (result.textContent = 'Please input a quantity and ingredients');
+  }
+  // first thing - transform text to array delimited by "enter"
+  const listArr = text.split('\n');
+  const splitArr = listArr.map(ingredient => {
+    const split = ingredient.split(/(\d+)/).filter(x => x);
+    return split;
+  });
+  const newSplit = splitArr.map(item => {
+    const newQ = (parseFloat(item[0]) * parseFloat(quantity)).toString();
+    item[0] = newQ;
+    return item.join('');
+  });
+  console.log(newSplit);
+  const resultUl = document.createElement('ul');
+  newSplit.forEach(item => {
+    const li = document.createElement('li');
+    li.append(item);
+    resultUl.appendChild(li);
+  });
+  while (result.firstChild) {
+    result.removeChild(result.firstChild);
+  }
+  result.appendChild(resultUl);
+}
+
+calculateBtn.addEventListener('click', () =>
+  transformText(textArea.value, quantitySelector.value)
+);
