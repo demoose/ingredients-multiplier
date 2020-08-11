4f5c6a5df7993e1c67192f78a346d5556830e67a
demoose <mryuim@gmail.com>
2020-08-11 14:35:35 +0100

    Add copy buttons - restyle, rename title, add option for servings/feed

--- text.js
+++ text.js
@@ -2,39 +2,66 @@ const result = document.querySelector('.result');
 const textArea = document.querySelector('[name="text"]');
 const calculateBtn = document.querySelector('.submit');
 const quantitySelector = document.querySelector('[name="quantity"]');
+const servingSelector = document.querySelector('[name="servings"]');
+const feedSelector = document.querySelector('[name="feed"]');
+const copyButton = document.querySelector('#copy');
 
-function transformText(text, quantity) {
-  if (!quantity || !text) {
+function transformText(text, quantity, servings, feed) {
+  if (quantity || text || (servings && feed)) {
+    // first thing - transform text to array delimited by "enter"
+    const listArr = text.split('\n');
+    const splitArr = listArr.map(ingredient => {
+      const split = ingredient.split(/(\d+)/).filter(x => x);
+      return split;
+    });
+
+    const newSplit = splitArr.map(item => {
+      if (isNaN(item[0])) {
+        return item;
+      }
+      // debugger;
+      let newQ;
+      if (servings && feed) {
+        const feedQuantity = parseFloat(feed / servings);
+        newQ = (parseFloat(item[0]) * parseFloat(feedQuantity)).toFixed(2);
+      } else {
+        newQ = (parseFloat(item[0]) * parseFloat(quantity)).toFixed(2);
+      }
+      console.log(newQ);
+      item[0] = newQ;
+      return item.join('');
+    });
+
+    const resultUl = document.createElement('ul');
+    newSplit.forEach(item => {
+      const li = document.createElement('li');
+      li.append(item);
+      resultUl.appendChild(li);
+    });
+    while (result.firstChild) {
+      result.removeChild(result.firstChild);
+    }
+    result.appendChild(resultUl);
+    copyButton.classList.remove('copy');
+  } else {
     return (result.textContent = 'Please input a quantity and ingredients');
   }
-  // first thing - transform text to array delimited by "enter"
-  const listArr = text.split('\n');
-  const splitArr = listArr.map(ingredient => {
-    const split = ingredient.split(/(\d+)/).filter(x => x);
-    return split;
-  });
-
-  const newSplit = splitArr.map(item => {
-    if (isNaN(item[0])) {
-      return item;
-    }
-    const newQ = (parseFloat(item[0]) * parseFloat(quantity)).toString();
-    item[0] = newQ;
-    return item.join('');
-  });
-
-  const resultUl = document.createElement('ul');
-  newSplit.forEach(item => {
-    const li = document.createElement('li');
-    li.append(item);
-    resultUl.appendChild(li);
-  });
-  while (result.firstChild) {
-    result.removeChild(result.firstChild);
-  }
-  result.appendChild(resultUl);
 }
 
 calculateBtn.addEventListener('click', () =>
-  transformText(textArea.value, quantitySelector.value)
+  transformText(
+    textArea.value,
+    quantitySelector.value,
+    servingSelector.value,
+    feedSelector.value
+  )
 );
+
+const clipboard = new ClipboardJS(copyButton);
+clipboard.on('success', function(e) {
+  console.log(e);
+});
+
+clipboard.on('error', function(e) {
+  console.log(e);
+});
